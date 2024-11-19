a83c77a961578ded8dc6f9145aa88935182fc736
zserub <zserub1000@gmail.com>
2024-11-19 19:14:12 +0100

    add control

--- RGBdaylight_blynk_basic.py
+++ RGBdaylight_blynk_basic.py
@@ -5,11 +5,24 @@
 
 import BlynkLib
 import time
+#from BlynkTimer import BlynkTimer
+from rgb import RGB
 
 # Initialize Blynk
-blynk = BlynkLib.Blynk('YourAuthToken')
+blynk = BlynkLib.Blynk('asd')
 
-# Variables
+# rgb object configuration
+config={
+    "white_balance":[1,1,1],
+    "intensity": 1,
+    "led_pins": {"r":17, "g":27, "b":18}
+}
+rgb=RGB(config)
+
+# Init variables
+r=0
+g=0
+b=0
 automode = False
 start_time = 0
 stop_time = 0
@@ -19,34 +32,61 @@ def get_current_time():
     current_time = time.localtime()  # Get current time as a struct_time object
     return current_time.tm_hour * 3600 + current_time.tm_min * 60 + current_time.tm_sec
 
-@blynk.VIRTUAL_WRITE(3)
+# Led control through V0 virtual pin
+@blynk.on("V0")
+def v0_write_handler(value):
+    if automode == True:
+        return
+    global r
+    r=int(value[0])/100
+    rgb.color=[r, g, b]
+    print(f'Red value changed to {r}')
+
+# Led control through V1 virtual pin
+@blynk.on("V1")
+def v1_write_handler(value):
+    if automode == True:
+        return
+    global b
+    b=int(value[0])/100
+    rgb.color=[r, g, b]
+    print(f'Blue value changed to {b}')
+        
+# Led control through V2 virtual pin
+@blynk.on("V2")
+def v2_write_handler(value):
+    if automode == True:
+        return
+    global g
+    g=int(value[0])/100
+    rgb.color=[r, g, b]
+    print(f'Green value changed to {g}')
+    
+@blynk.on("V3")
 def v3_write_handler(value):
     global automode
-    if value == 1:  # Auto mode
+    if int(value[0]) == 1:  # Auto mode
         automode = True
         print("Auto mode activated")
     else:  # Manual mode
         automode = False
-        print("Manual mode activated")
+        print("Manual mode activated")    
 
-
-@blynk.VIRTUAL_WRITE(4)
+@blynk.on("V4")
 def v4_time_handler(value):
     global start_time, stop_time
     start_time = value[0]  # start time in seconds
     stop_time = value[1]
     print('Start Time: {}, Stop Time: {}'.format(start_time, stop_time))
-
-
+    
 def sunrisesim():
     # rgb object
     pass
 
-
+#function to sync the data from virtual pins
 @blynk.on("connected")
 def blynk_connected():
-    print("Raspberry Pi Connected to Blynk")
-
+    print("Raspberry Pi Connected to Blynk") 
 
 while True:
     blynk.run()
